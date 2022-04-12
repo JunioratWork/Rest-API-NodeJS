2509a6804a8f5c2c7ed32dbb5dc709f3e063d7b2
Junior Acosta <jacosta.atwork@gmail.com>
2022-04-12 17:57:02 -0500

    cambios al servidor

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,12 @@
 const { Router } = require("express");
 const router = Router();
 
-router.get('/test', (req, res) => {
+router.get('/', (req, res) => {
     const data = {
         "name": "Fazt",
         "Edad": "31"
     };
-    res.json(data);
+    res.json("Hola esta es una prueba")
 });
 
 module.exports = router;
