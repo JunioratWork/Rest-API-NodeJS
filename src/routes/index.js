d708f7ebcc2f2cb25d6c7f5f547201b01cdc1d09
Junior Acosta <jacosta.atwork@gmail.com>
2022-04-12 18:07:41 -0500

    actualizacion

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,7 @@ router.get('/', (req, res) => {
         "name": "Fazt",
         "Edad": "31"
     };
-    res.json("Hola esta es una prueba")
+    res.json("Hola esta es una prueba para ver si se sube algo")
 });
 
 module.exports = router;
