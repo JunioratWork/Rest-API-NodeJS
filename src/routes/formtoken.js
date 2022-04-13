8a6a5991e41acc0e59d96c71dc4555dd339e0d64
Junior Acosta <jacosta.atwork@gmail.com>
2022-04-13 16:06:04 -0500

    adecuacion para que reciba valores nulos

--- src/routes/formtoken.js
+++ src/routes/formtoken.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const router = Router();
 const request = require('request');
+
 /*
 router.post('/', (req, res) => {
     console.log(req.body);
@@ -21,23 +22,43 @@ router.post('/', (req, res) => {
 };*/
 
 //router.post('/init', (req, res) => {
-router.post('/init', function(req, res, next) {
-
+router.post('/init', function(req, res, next) 
+{
   var order = req.body;
+  
+  console.log("Este es la informacion del BODY antes de entrar a los IF: ");
+  console.log(isEmptyObject(order));
+  
+  if(isEmptyObject(order) === true || isEmptyObject(order) === 'undefined'){
+      order = {
+        "amount":   180,
+        "currency": "PEN",
+        "orderId":  "myOrderId-999999",
+        "customer": {
+            "email": "sample@example.com"
+        }
+      };    
+      console.log("Este es la informacion del BODY en el IF: ");
+      console.log(order);
+  }
+  else{
+    order = req.body;
+    console.log("Este es la informacion del BODY en el ELSE: ");
+    console.log(order);
+  } 
 
-  if(order==null){
-    order = {
-      "amount":   180,
-      "currency": "PEN",
-      "orderId":  "myOrderId-999999",
-      "customer": {
-          "email": "sample@example.com"
-      }
-  };
+  //comprueba si el body esta vacio, devuelve true si esta vacio
+  function isEmptyObject(obj) {
+    for (var property in obj) {
+        if (obj.hasOwnProperty(property)) {
+            return false;
+        }
+    }
+ 
+    return true;
   }
 
-  // Call CreatePayment web service to create the form token
-  
+  // Call CreatePayment web service to create the form token 
   request.post({
     url: "https://api.micuentaweb.pe/api-payment/V4/Charge/CreatePayment",
     headers: {
@@ -45,7 +66,9 @@ router.post('/init', function(req, res, next) {
       'Content-Type': 'application/json'
     },
     json: order
-  }, function(error, response, body) {
+  }, 
+  function(error, response, body) {
+
     if (body.status === 'SUCCESS')
     {
       // Send back the form token to the client side
@@ -57,7 +80,8 @@ router.post('/init', function(req, res, next) {
       // Do your own error handling  
       console.error(body);
       res.status(500).send('error');
-    }
+    }  
+
   });
 });
 
