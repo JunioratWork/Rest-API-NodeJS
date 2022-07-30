8aaa8c2a7986185c70db1f8c652a8e5baf6c527f
Junior Acosta <89037989+JunioratWork@users.noreply.github.com>
2022-07-30 03:42:35 -0500

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const router = Router();
 
-router.get('/', (req, res) => {
+router.post('/', (req, res) => {
     const data = {
         "name": "Fazt",
         "Edad": "31"
