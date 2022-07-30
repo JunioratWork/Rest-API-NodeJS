684010ad17ea0eea99075e2c09eb9b5a16147ae4
Junior Acosta <89037989+JunioratWork@users.noreply.github.com>
2022-07-30 03:49:15 -0500

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,18 +8,11 @@ router.post('/', (req, res) => {
     };
     res.json("Hola este es un cambio")
     
-    if (body.status === 'SUCCESS')
-    {
+    
       // Send back the form token to the client side
-      const datos = body.kr-answer;
+      const datos = kr-answer;
       res.send(datos);
-    }
-    else
-    {
-      // Do your own error handling  
-      console.error(body);
-      res.status(500).send('error');
-    }  
+    
 });
 
 module.exports = router;
