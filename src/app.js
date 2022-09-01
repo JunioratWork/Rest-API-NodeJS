6f33383408e842a6936c5ca27cddde2d7d3c44ea
Junior Acosta <89037989+JunioratWork@users.noreply.github.com>
2022-09-01 12:14:00 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 //Habilitar bloqueo de CORS
 const cors = require('cors');
-const lista= ['http://localhost:3000','https://izipay-ejemplo-php.000webhostapp.com','https://prueba55.herokuapp.com','http://127.0.0.1.com'];
+const lista= ['http://localhost:3000','https://izipay-ejemplo-php.000webhostapp.com','https://prueba55.herokuapp.com','127.0.0.1:5501'];
 app.use(cors({origin: lista}));
 
 //Confiuguracion del puerto
